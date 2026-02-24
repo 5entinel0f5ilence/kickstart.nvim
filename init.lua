9f623c2641f5bee89224aadf0017c9d98b780506
5entinel <5entinel0f5ilence@proton.me>
2026-02-24 04:05:35 +0300

    set 2-space indentation for bash/shell scripts autoformatting

--- init.lua
+++ init.lua
@@ -765,11 +765,20 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        sh = { 'shfmt' },
+        bash = { 'shfmt' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+      formatters = {
+        -- Set bash and shell indentation
+        shfmt = {
+          -- Pass the -i 2 argument to force 2-space indentation
+          args = { '-i', '2', '-bn', '-ci', '-sr' },
+        },
       },
     },
   },
